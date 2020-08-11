5ba59b7d13fbf62aca54068200d5bc9cb96cccc8
chanasorn1234 <69181563+chanasorn1234@users.noreply.github.com>
2020-08-11 14:34:43 +0700

    Add files via upload

---
+++ Programing1.1.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{	
+	for (int i = 1; i <= 10; i++)
+	{
+		if (i % 2 == 0)
+		{
+			printf("i is even number\n");
+
+
+		}
+
+		if (i % 2 != 0)
+		{
+			printf("i is odd number\n");
+
+
+		}
+
+		
+	}
+	
+	
+	printf("End");
+
+
+	return 0;
+}
