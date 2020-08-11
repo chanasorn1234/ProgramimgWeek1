2851c898c982c7479014a28dfe35972362242d52
chanasorn1234 <69181563+chanasorn1234@users.noreply.github.com>
2020-08-11 15:17:41 +0700

    Add files via upload

---
+++ Programimg1.2.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int Hello()
+{
+	for (int n = 1; n <= 10; n++)
+	{	
+		printf("Sawassdekub");
+		for (int i = 1; i <= 10; i++)
+		{
+
+			printf("Chanasorn Siriwong\n");
+			printf("0153\n");
+		}
+
+	}
+	return 0;
+}
